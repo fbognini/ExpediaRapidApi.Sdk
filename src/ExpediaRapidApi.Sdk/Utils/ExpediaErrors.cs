f6442c9bc0853ac1b381f36f900eeab5c2702f1d
Francesco Bognini <info@francescobognini.com>
2023-10-02 16:56:32 +0200

    expedia errors

---
+++ src/ExpediaRapidApi.Sdk/Utils/ExpediaErrors.cs
@@ -0,0 +1,21 @@
+﻿using ExpediaRapidApi.Sdk.Models;
+using fbognini.Sdk.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExpediaRapidApi.Sdk.Utils
+{
+    public static class ExpediaErrors
+    {
+        public static ExpediaErrorModel ConvertApiExceptionToErrorModel(ApiException exception)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(exception.Response, nameof(exception));
+
+            return JsonSerializer.Deserialize<ExpediaErrorModel>(exception.Response)!;
+        }
+    }
+}
