697ba9161e371ce64f8b274c0263c5f167417379
Francesco Bognini <info@francescobognini.com>
2023-10-02 17:42:31 +0200

    .

---
+++ src/ExpediaRapidApi.Sdk/Models/Errors/ExpediaError.cs
@@ -0,0 +1,50 @@
+﻿using fbognini.Sdk.Exceptions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ExpediaRapidApi.Sdk.Models
+{
+    public class ExpediaError
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = string.Empty;
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+        [JsonPropertyName("errors")]
+        public List<ExpediaError> Errors { get; set; } = new();
+        [JsonPropertyName("fields")]
+        public List<ErrorField> Fields { get; set; } = new();
+
+        public static bool TryParse(ApiException exception, out ExpediaError expediaError)
+        {
+            var _error = ConvertApiExceptionToErrorModel(exception);
+            if (_error == null)
+            {
+                expediaError = new();
+                return false;
+            }
+
+            expediaError = _error!;
+            return true;
+        }
+
+        public static ExpediaError Parse(ApiException exception)
+        {
+            if (TryParse(exception, out ExpediaError error))
+            {
+                return error;
+            }
+
+            throw new FormatException();
+        }
+
+        private static ExpediaError? ConvertApiExceptionToErrorModel(ApiException exception)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(exception.Response, nameof(exception));
+
+            return JsonSerializer.Deserialize<ExpediaError>(exception.Response);
+        }
+
+
+    }
+}
--- src/ExpediaRapidApi.Sdk/Models/Properties/PropertyAvailabilityResponse.cs
+++ src/ExpediaRapidApi.Sdk/Models/Properties/PropertyAvailabilityResponse.cs
@@ -2,15 +2,7 @@
 
 namespace ExpediaRapidApi.Sdk.Models.Properties
 {
-    public class PropertyAvailabilityResponse : List<PropertyAvailability>, IExpediaErrorModel
+    public class PropertyAvailabilityResponse : List<PropertyAvailability>
     {
-        [JsonPropertyName("type")]
-        public string? Type { get; set; }
-        [JsonPropertyName("message")]
-        public string? Message { get; set; }
-        [JsonPropertyName("errors")]
-        public List<ExpediaErrorModel>? Errors { get; set; }
-        [JsonPropertyName("fields")]
-        public List<ErrorField>? Fields { get; set; }
     }
 }
