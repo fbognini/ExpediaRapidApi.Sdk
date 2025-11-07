bebab6bf6efb8718f274c6fc38b8659bf4fa2f9e
Francesco Bognini <info@francescobognini.com>
2025-11-07 15:54:12 +0100

    Different cancel penalty from Car and Lodging

---
+++ src/ExpediaRapidApi.Sdk/Cars/Shared/CarCancelPenality.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpediaRapidApi.Sdk.Cars.Shared;
+
+public class CarCancelPenality
+{
+    /// <summary>
+    /// The time after which the cancellation penalty applies, provided in ISO 8601 format. If start_time is not provided, the cancellation penalty applies from the time of booking until the next start_time or the rental pickup_time, whichever occurs first.
+    /// </summary>
+    public DateTime? Start { get; set; }
+    /// <summary>
+    /// The time before which the cancellation penalty applies, provided in ISO 8601 format. If end_time is not provided, the cancellation penalty applies until the start of the rental period.
+    /// </summary>
+    public DateTime? End { get; set; }
+    public double? Amount { get; set; }
+    public string? Currency { get; set; }
+    /// <summary>
+    /// Percentage of total booking charged for as penalty. A thirty percent penalty would be returned as 30%
+    /// </summary>
+    public string? Percent { get; set; }
+}
--- src/ExpediaRapidApi.Sdk/Lodging/Shared/LodgingCancelPenality.cs
+++ src/ExpediaRapidApi.Sdk/Lodging/Shared/LodgingCancelPenality.cs
@@ -5,23 +5,23 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace ExpediaRapidApi.Sdk.Shared;
+namespace ExpediaRapidApi.Sdk.Lodging.Shared;
 
-public class CancelPenalty
+public class LodgingCancelPenality
 {
-    [JsonPropertyName("start")]
+    /// <summary>
+    /// Effective date and time of cancellation penalty in extended ISO 8601 format, with ±hh:mm timezone offset
+    /// </summary>
     public DateTimeOffset Start { get; set; }
-    [JsonPropertyName("end")]
+    /// <summary>
+    /// End date and time of cancellation penalty in extended ISO 8601 format, with ±hh:mm timezone offset
+    /// </summary>
     public DateTimeOffset End { get; set; }
-    [JsonPropertyName("nights")]
     public string? Nights { get; set; }
-    [JsonPropertyName("amount")]
     public double? Amount { get; set; }
-    [JsonPropertyName("currency")]
     public string? Currency { get; set; }
     /// <summary>
     /// Percentage of total booking charged for as penalty. A thirty percent penalty would be returned as 30%
     /// </summary>
-    [JsonPropertyName("percent")]
     public string? Percent { get; set; }
 }
